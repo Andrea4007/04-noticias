c48d25bb1d7f8f7283624b650b03bf5bbac02825
Andrea4007 <16980824@itroque.edu.mx>
2020-03-11 17:44:20 -0600

    pagina de encabezados finish

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild} from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
+  //colocarle un valor por defecto al ion segment
+  @ViewChild(IonSegment) segment: IonSegment;
+
+  categorias = ["business", "entertainment", "general", "health", "science", "sports", "technology"];
+ 
   constructor() {}
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.segment.value=this.categorias[0];
+  }
 
 }
